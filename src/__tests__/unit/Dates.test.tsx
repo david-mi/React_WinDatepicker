07f015ee138124da7011b8b728fdd0bb089353b0
David Michel <david.michel2809@gmail.com>
2023-05-01 13:59:37 +0200

    test: add unit test for Dates component #3

---
+++ src/__tests__/unit/Dates.test.tsx
@@ -0,0 +1,22 @@
+import { expect } from "vitest";
+import { render, screen, getNodeText } from "@testing-library/react";
+import Dates from "../../lib/WinDate/Month/Dates/Dates";
+import { getDates } from "../../lib/WinDate/Month/helper";
+
+describe("<Dates>", () => {
+  it("Should page with all getDates values from getDates", () => {
+    const date = new Date(2023, 4, 1)
+    const dates = getDates(date)
+    const getDatesValuesAsString: string[] = dates.map((date) => {
+      return String(date.getDate)
+    })
+    const expectedLength = 42
+
+    render(<Dates dates={dates} />)
+    const datesElements = screen.getAllByTestId("date")
+    const dateElementsText = datesElements.map(getNodeText)
+
+    expect(datesElements).toHaveLength(expectedLength)
+    expect(dateElementsText).toEqual(getDatesValuesAsString)
+  })
+})
