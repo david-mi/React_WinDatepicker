37dfad3cfec0150a74d671fa80e7799b22f4e43f
David Michel <david.michel2809@gmail.com>
2023-05-01 18:53:42 +0200

    feat: add custom icon to hide the default one and prevent opening the default calendar with keyboard #9

--- src/lib/WinDate/DateInput/DateInput.tsx
+++ src/lib/WinDate/DateInput/DateInput.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef, useContext, MouseEvent } from "react"
+import { useEffect, useRef, useContext, KeyboardEvent } from "react"
 import { GlobalContext } from "../../Context/Global"
 import type { ChangeEvent } from "react"
 import { formatDate } from "../Calendar/Month/helper"
+import styles from "./dateInput.module.css"
+import CalendarIcon from "./CalendarIcon/CalendarIcon"
 
 const DateInput = () => {
   const dateInputRef = useRef<HTMLInputElement>(null!)
@@ -22,24 +24,37 @@ const DateInput = () => {
     setDate(newDate)
   }
 
-  function handleClick(event: MouseEvent<HTMLInputElement>) {
-    event.preventDefault()
-    openCalendar()
-  }
-
   useEffect(() => {
     dateInputRef.current.value = formatDate(date)
   }, [date])
 
+  /**
+   * Open calendar component instead of opening the navigator default calendar 
+   * when pressing enter on focused calendar icon
+   */
+
+  function preventEnterKey(event: KeyboardEvent) {
+    const key = event.key
+
+    if (key === "Enter") {
+      event.preventDefault()
+      openCalendar()
+    }
+  }
+
   return (
-    <input
-      type="date"
-      name="date"
-      id="date"
-      onChange={handleChange}
-      onClick={handleClick}
-      ref={dateInputRef}
-    />
+    <div className={styles.container}>
+      <input
+        type="date"
+        name="date"
+        id="date"
+        onChange={handleChange}
+        ref={dateInputRef}
+        onClick={(e) => e.preventDefault()}
+        onKeyDown={preventEnterKey}
+      />
+      <CalendarIcon className={styles.icon} openCalendar={openCalendar} />
+    </div>
   )
 }
 export default DateInput
