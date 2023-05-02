68c54a848930868b22345ee85947c12c6d089ad1
David Michel <david.michel2809@gmail.com>
2023-05-02 17:06:33 +0200

    fix: little fix on Dates test describe #3

--- src/__tests__/unit/Dates.test.tsx
+++ src/__tests__/unit/Dates.test.tsx
@@ -4,7 +4,7 @@ import Dates from "../../lib/WinDate/Calendar/Month/Dates/Dates";
 import { getDates } from "../../lib/WinDate/Calendar/Month/helper";
 
 describe("<Dates>", () => {
-  it("Should page with all getDates values from getDates", () => {
+  it("Should retrieve all getDate values in the document", () => {
     const date = new Date(2023, 4, 1)
     const dates = getDates(date)
     const getDatesValuesAsString: string[] = dates.map((date) => {
