d1cbb37c347aeae1ebabbf123012867cf2fc4452
David Michel <david.michel2809@gmail.com>
2023-05-08 08:58:20 +0200

    refacto: improve getDates helper

--- src/lib/WinDate/Calendar/Month/helper.ts
+++ src/lib/WinDate/Calendar/Month/helper.ts
@@ -14,8 +14,9 @@ const getDayIndex = {
 type DaysIndexKeys = keyof typeof getDayIndex
 
 /**
- * Retrieve all 42 dates to display on a calendar page
+ * Retrieve all 126 dates to display on a calendar page
  * 
+ * - Add previous / current / next
  * - If the first day of the month from the {@link chosenDate} does not
  *   start with monday, add the last dates from the previous month to compensate
  * - Add all dates from the {@link chosenDate} month
@@ -26,50 +27,38 @@ export function getDates(chosenDate: Date): DatesFormat[] {
   /** make a copy of {@link chosenDate} to not mutate it */
   const date = new Date(chosenDate)
 
-  const monthDates = []
+  const dates = []
   const firstDateOfTheMonth = 1
   const chosenMonth = date.getMonth()
 
-  const previousMonth = new Date(date)
-  previousMonth.setMonth(chosenMonth - 1)
-
-  const nextMonth = new Date(date)
-  nextMonth.setMonth(chosenMonth + 1)
-
-  const monthToStop = new Date(nextMonth)
-  monthToStop.setMonth(date.getMonth() + 3)
-
-  const monthToStart = new Date(date)
-  monthToStart.setMonth(date.getMonth() - 2)
-  const yearToStart = monthToStart.getFullYear()
-
-  const afterNextMonth = new Date(date);
-  afterNextMonth.setMonth(date.getMonth() + 2);
-
-  date.setFullYear(yearToStart)
-  date.setMonth(monthToStart.getMonth())
+  date.setMonth(date.getMonth() - 1)
   date.setDate(firstDateOfTheMonth)
   const firstDayOfTheMonth = date.getDay()
 
   const amountOfDatesToGetFromPreviousMonth = getDayIndex[firstDayOfTheMonth as DaysIndexKeys]
   date.setDate(firstDateOfTheMonth - amountOfDatesToGetFromPreviousMonth)
 
+  const numberOfDatesToAdd = 112
+  let addedDatesCount = 0
 
   let dayDate: number
 
-  while (date.getMonth() !== monthToStop.getMonth()) {
+  while (addedDatesCount < numberOfDatesToAdd) {
     dayDate = date.getDate()
-    monthDates.push({
+
+    dates.push({
       getDate: dayDate,
       getFormatedDate: formatDate(date),
       isFromChosenMonth: date.getMonth() === chosenMonth,
       isToday: areDatesIdentical(new Date(), date),
       isChosenDate: areDatesIdentical(chosenDate, date),
     })
+
     date.setDate(dayDate + 1)
+    addedDatesCount++
   }
-  console.log(monthDates.length)
-  return monthDates
+
+  return dates
 }
 
 /**
