79eb0ec9dd8288bcc837c13d49316a0e0b43c9e8
David Michel <david.michel2809@gmail.com>
2023-05-12 15:14:43 +0200

    feat: add d.ts file

---
+++ src/lib/index.d.ts
@@ -0,0 +1,42 @@
+import type { ComponentProps } from "react";
+type Color = string;
+export interface CalendarColors {
+    /** background color behind calendar */
+    background?: Color;
+    closeBackground?: Color;
+    headerButtonsText?: Color;
+    headerButtonsTextHover?: Color;
+    weekDaysText?: Color;
+    chosenButtonText?: Color;
+    /** border color of chosen day / month */
+    chosenBorder?: Color;
+    /** text color of today / current month */
+    currentButtonBackground?: Color;
+    currentButtonBorder?: Color;
+    currentButtonSecondBorder?: Color;
+    notCurrentButtonText?: Color;
+    buttonBorderHover?: Color;
+}
+export type OnDateChangeProps = {
+    /** updated Date formatted to "YYYY-MM-DD" string */
+    formatted: string | "";
+    /** updated Date */
+    raw: Date | null;
+};
+export interface Props {
+    /** Custom colors to apply on calendar */
+    calendarColors?: CalendarColors;
+    /**
+     * Props to apply on date input
+     *
+     * - accept all react input props and a onDateChange callback
+     */
+    inputProps: ComponentProps<"input"> & {
+        /** Gets called whenever date gets updated from input or calendar */
+        onDateChange: (date: OnDateChangeProps) => any;
+        /** sets input value to today by default, set to false by default */
+        setTodayByDefault: boolean;
+    };
+}
+export declare const WinDateWrapper: (props: Props) => JSX.Element;
+export {};
