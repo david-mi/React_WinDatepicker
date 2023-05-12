7ffb7ac7f949eab16b36c634aee134a689242f65
David Michel <david.michel2809@gmail.com>
2023-05-12 08:28:01 +0200

    feat: add button positioning logic on Button component

--- src/lib/WinDate/DateInput/Button/Button.tsx
+++ src/lib/WinDate/DateInput/Button/Button.tsx
@@ -1,14 +1,22 @@
-import { useContext, forwardRef } from "react"
+import { useContext, useRef, forwardRef, useLayoutEffect, MutableRefObject } from "react"
 import { GlobalContext } from "../../../Context/Global"
 import styles from "./button.module.css"
 
-const Button = forwardRef<HTMLButtonElement>((_, buttonRef) => {
+const Button = forwardRef<HTMLInputElement>((_, dateInputRef) => {
   const { setTimeline, openCalendar } = useContext(GlobalContext)
+  const buttonRef = useRef<HTMLButtonElement>(null!)
 
   function handleClick() {
     setTimeline("MONTH")
     openCalendar()
   }
+
+  useLayoutEffect(() => {
+    const inputComputedStyle = getComputedStyle((dateInputRef as MutableRefObject<HTMLInputElement>).current)
+    const inputPaddingLeft = inputComputedStyle.getPropertyValue("padding-right")
+
+    buttonRef.current.style.right = inputPaddingLeft
+  }, [dateInputRef])
 
   return (
     <button
