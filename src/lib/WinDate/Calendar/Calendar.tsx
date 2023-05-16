e72b3d1bf71ee77e0511c22f081e05114cffe910
David Michel <david.michel2809@gmail.com>
2023-05-16 15:49:34 +0200

    fix: move root colors assignation to calendar component to avoid colors issues when working with multiple calendars

--- src/lib/WinDate/Calendar/Calendar.tsx
+++ src/lib/WinDate/Calendar/Calendar.tsx
@@ -4,14 +4,16 @@ import styles from "./calendar.module.css"
 import Month from "./Month/Month"
 import Year from "./Year/Year"
 import type { Timeline } from "../../Context/Global"
+import type { CalendarColors } from "../.."
 import { defineCalendarPosition } from "../helper"
 
 interface Props {
   dateInputRef: MutableRefObject<HTMLInputElement>
   winDateRef: MutableRefObject<HTMLDivElement>
+  calendarColors?: CalendarColors
 }
 
-const Calendar = ({ dateInputRef, winDateRef }: Props) => {
+const Calendar = ({ dateInputRef, winDateRef, calendarColors = {} }: Props) => {
   const { closeCalendar, timeline, calendarPosition } = useContext(GlobalContext)
   const calendarRef = useRef<HTMLDivElement>(null!)
 
@@ -21,6 +23,31 @@ const Calendar = ({ dateInputRef, winDateRef }: Props) => {
     "MONTH": <Month />,
     "YEAR": <Year />
   }
+
+  const calendarColorsRoot = [
+    ["--calendar-bg", calendarColors.background || "#1f1f1f"],
+    ["--calendar-header-buttons-clr", calendarColors.headerButtonsText || "#cdcdcd"],
+    ["--calendar-header-buttons-clr-hover", calendarColors.headerButtonsTextHover || "white"],
+    ["--calendar-weekdays-clr", calendarColors.weekDaysText || "white"],
+    ["--calendar-chosen-clr", calendarColors.chosenButtonText || "white"],
+    ["--calendar-chosen-border", calendarColors.chosenBorder || "#8c8c8c"],
+    ["--calendar-current-bg", calendarColors.currentButtonBackground || "#3f3f3f"],
+    ["--calendar-current-border", calendarColors.currentButtonBorder || "#3f3f3f"],
+    ["--calendar-current-border-inner", calendarColors.currentButtonSecondBorder || "black"],
+    ["--calendar-not-current-clr", calendarColors.notCurrentButtonText || "#797979"],
+    ["--calendar-border-hover", calendarColors.buttonBorderHover || "#8c8c8c"],
+    ["--calendar-disabled-clr", calendarColors.disabled || "rgba(185, 185, 185, 0.089)"],
+    ["--calendar-disabled-bg", calendarColors.disabledBackground || "rgba(128, 128, 128, 0.068)"]
+  ]
+
+  useLayoutEffect(() => {
+    /** Applies css root colors values for calendar, based on calendarColors props */
+
+    calendarColorsRoot.forEach(([rootProperty, color]) => {
+      document.documentElement.style.setProperty(rootProperty, color)
+    })
+  }, [calendarColors])
+
 
   useLayoutEffect(() => {
     /** 
@@ -44,6 +71,14 @@ const Calendar = ({ dateInputRef, winDateRef }: Props) => {
     }
   }, [])
 
+  useEffect(() => {
+    document.addEventListener("click", handleClick)
+
+    return () => {
+      document.removeEventListener("click", handleClick)
+    }
+  }, [])
+
   /**
    * Close calendar only if we click outstide winDateRef div scope
    * 
@@ -60,14 +95,6 @@ const Calendar = ({ dateInputRef, winDateRef }: Props) => {
       closeCalendar()
     }
   }
-
-  useEffect(() => {
-    document.addEventListener("click", handleClick)
-
-    return () => {
-      document.removeEventListener("click", handleClick)
-    }
-  }, [])
 
   return (
     <div
--- src/lib/WinDate/WinDate.tsx
+++ src/lib/WinDate/WinDate.tsx
@@ -1,11 +1,11 @@
-import { useContext, useLayoutEffect, useRef } from "react"
+import { useContext, useRef } from "react"
 import { GlobalContext } from "../Context/Global"
 import Calendar from "./Calendar/Calendar"
 import DateInput from "./DateInput/DateInput"
 import styles from "./winDate.module.css"
 import type { Props } from "../index"
 
-const Windate = ({ calendarColors = {}, inputProps }: Props) => {
+const Windate = ({ calendarColors, inputProps }: Props) => {
   const { isCalendarOpen } = useContext(GlobalContext)
   const winDateRef = useRef<HTMLDivElement>(null!)
   const dateInputRef = useRef<HTMLInputElement>(null!)
@@ -15,33 +15,15 @@ const Windate = ({ calendarColors = {}, inputProps }: Props) => {
     return null
   }
 
-  const calendarColorsRoot = [
-    ["--calendar-bg", calendarColors.background || "#1f1f1f"],
-    ["--calendar-header-buttons-clr", calendarColors.headerButtonsText || "#cdcdcd"],
-    ["--calendar-header-buttons-clr-hover", calendarColors.headerButtonsTextHover || "white"],
-    ["--calendar-weekdays-clr", calendarColors.weekDaysText || "white"],
-    ["--calendar-chosen-clr", calendarColors.chosenButtonText || "white"],
-    ["--calendar-chosen-border", calendarColors.chosenBorder || "#8c8c8c"],
-    ["--calendar-current-bg", calendarColors.currentButtonBackground || "#3f3f3f"],
-    ["--calendar-current-border", calendarColors.currentButtonBorder || "#3f3f3f"],
-    ["--calendar-current-border-inner", calendarColors.currentButtonSecondBorder || "black"],
-    ["--calendar-not-current-clr", calendarColors.notCurrentButtonText || "#797979"],
-    ["--calendar-border-hover", calendarColors.buttonBorderHover || "#8c8c8c"],
-    ["--calendar-disabled-clr", calendarColors.disabled || "rgba(185, 185, 185, 0.089)"],
-    ["--calendar-disabled-bg", calendarColors.disabledBackground || "rgba(128, 128, 128, 0.068)"]
-  ]
-
-  useLayoutEffect(() => {
-    /** Applies css root colors values for calendar, based on calendarColors props */
-
-    calendarColorsRoot.forEach(([rootProperty, color]) => {
-      document.documentElement.style.setProperty(rootProperty, color)
-    })
-  }, [calendarColors])
-
   return (
     <div className={styles.winDate} ref={winDateRef}>
-      {isCalendarOpen && <Calendar winDateRef={winDateRef} dateInputRef={dateInputRef} />}
+      {isCalendarOpen && (
+        <Calendar
+          winDateRef={winDateRef}
+          dateInputRef={dateInputRef}
+          calendarColors={calendarColors}
+        />
+      )}
       <DateInput inputProps={inputProps} dateInputRef={dateInputRef} />
     </div>
   )
