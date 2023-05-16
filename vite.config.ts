628d65276ff86464cbd1e142dca69c57c02b735b
David Michel <david.michel2809@gmail.com>
2023-05-16 18:33:56 +0200

    config: add build config for lib and for doc

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,37 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
-export default defineConfig({
-  plugins: [react(), libInjectCss()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: "./tests.setup.ts"
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/lib/index.tsx"),
-      name: "react-windate",
-      fileName: "index"
-    },
-    outDir: "dist",
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+  if (mode == "lib") {
+    return {
+      plugins: [react(), libInjectCss()],
+      build: {
+        lib: {
+          entry: resolve(__dirname, "src/lib/index.tsx"),
+          name: "react-windate",
+          fileName: "index"
+        },
+        outDir: "dist",
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
         },
       },
+    }
+  }
+
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: "./tests.setup.ts"
     },
-  },
+  }
 })
