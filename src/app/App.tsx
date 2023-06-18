be6f07f0419e2ad4dfb271682b0bc663136f1a0b
David Michel <david.michel2809@gmail.com>
2023-06-18 10:17:36 +0200

    config: handle lib or doc dev environment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
-// import Testing from "./Testing/Testing"
+import Testing from "./Testing/Testing"
 import Documentation from "./Documentation/Documentation"
 
+const envComponent = {
+  "doc": <Documentation />,
+  "lib": <Testing />
+}
+const envMode = import.meta.env.MODE as keyof typeof envComponent
+
 const App = () => {
-  return (
-    // <Testing />
-    <Documentation />
-  )
+  return envComponent[envMode]
 }
 
 export default App
